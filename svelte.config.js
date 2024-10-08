9b319d4101481fbebf38651ddda26f21fd9e0695
Julian Boaz <julianboaz@Mac.attlocal.net>
2024-10-07 22:23:15 -0400

    Fixing build job

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { assets } from '$app/paths';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,7 +12,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/investment-calculator' : '',
+    }
 	}
 };
 
